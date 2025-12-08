8e7150b3c7f138029dccf60711bd98cb9857dae1
Grimfrost Mage <180069990+grimfrostmage@users.noreply.github.com>
2025-12-07 18:49:27 -0600

    handle skip for windows and issue with bun treating skip and an internal nested test which it doesn't support

--- jsr/fs/chmod.test.ts
+++ jsr/fs/chmod.test.ts
@@ -1,5 +1,5 @@
 import { test } from "node:test";
-import { equal } from "@frostyeti/assert";
+import { equal, ok } from "@frostyeti/assert";
 import { chmod, chmodSync } from "./chmod.ts";
 import { join } from "@frostyeti/path";
 import { WIN } from "./globals.ts";
@@ -9,7 +9,21 @@ import { ensureFile, ensureFileSync } from "./ensure_file.ts";
 
 const testFile = join(import.meta.dirname!, "chmod_test.txt");
 
-test("fs::chmod changes permissions async", { skip: WIN }, async () => {
+const o: test.TestOptions = {};
+const g: Record<string, unknown> = globalThis as Record<string, unknown>;
+if (!g.Bun && WIN) {
+    o.skip = true;
+}
+
+test("fs::chmod changes permissions async", o, async () => {
+    if (g.Bun && WIN) {
+        ok(
+            true,
+            "Skipping test: Bun on Windows does not support nested tests using node:test, including the skip",
+        );
+        return;
+    }
+
     await ensureFile(testFile);
 
     try {
@@ -24,7 +38,15 @@ test("fs::chmod changes permissions async", { skip: WIN }, async () => {
     }
 });
 
-test("fs::chmodSync changes permissions sync", { skip: WIN }, async () => {
+test("fs::chmodSync changes permissions sync", o, async () => {
+    if (g.Bun && WIN) {
+        ok(
+            true,
+            "Skipping test: Bun on Windows does not support nested tests using node:test, including the skip",
+        );
+        return;
+    }
+
     ensureFileSync(testFile);
 
     try {
